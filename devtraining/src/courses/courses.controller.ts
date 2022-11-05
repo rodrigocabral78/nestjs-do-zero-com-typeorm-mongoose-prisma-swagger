044aa9b216051a2e6e9abfaae77aad53e52152d2
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2022-11-04 23:05:28 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	modificado:    src/courses/courses.controller.ts
    	modificado:    src/courses/courses.service.ts
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, Response } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
@@ -8,35 +8,31 @@ export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.coursesService.create(createCourseDto);
   }
 
   @Get()
-  findAll(@Response() response) {
-    const courses = this.coursesService.findAll();
-    return response.status(200).send(courses);
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
   // findOne(@Param('id') id: string) {
   findOne(@Param('id') id: string) {
-    return `Curso #${id}`;
+    return this.coursesService.findOne(id);
     // return this.coursesService.findOne(+id);
   }
 
   @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
-  update(@Param('id') id: string, @Body() body) {
-    return `Atualizacao do Curso #${id}`;
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
     // return this.coursesService.update(+id, updateCourseDto);
   }
 
   @Delete(':id')
-  // remove(@Param('id') id: string) {
   remove(@Param('id') id: string) {
-    return `Exclusao do Curso #${id}`;
+    return this.coursesService.remove(id);
     // return this.coursesService.remove(+id);
   }
 }
--- devtraining/src/courses/courses.service.ts
+++ devtraining/src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { Course } from './entities/course.entity';
@@ -27,7 +27,15 @@ export class CoursesService {
 
   // findOne(id: number) {
   findOne(id: string) {
-    return this.courses.find((course) => course.id === Number(id));
+    const course = this.courses.find((course) => course.id === Number(id));
+
+    if (!course) {
+      throw new HttpException(
+        `Course ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return course;
     // return `This action returns a #${id} course`;
   }
 
